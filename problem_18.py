a9d7a3472283b28f939dd5597653167f0df27061
= <=>
2022-06-17 01:55:41 -0400

    Build triangle

--- problem_18.py
+++ problem_18.py
@@ -1,1 +1,9 @@
 #Problem 18, solve a triangle
+
+#Build the triangle
+triangle = []
+with open("problem_18.txt",'r') as f:
+    for l in f.readlines():
+        triangle.append([int(x) for x in l.strip().split()])
+for line in triangle:
+    print(line)
