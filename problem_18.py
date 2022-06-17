f72124154e9f12d88849b0d4e1d162be6922b102
= <=>
2022-06-17 02:07:18 -0400

    Solved problem 18

--- problem_18.py
+++ problem_18.py
@@ -8,7 +8,18 @@ with open("problem_18.txt",'r') as f:
 
 #Calculate best route for triangle
 top = 0
-bottom = len(triangle)
+bottom = len(triangle) - 1
 current_level = bottom - 1
 while current_level >= top:
-    None
+    lower_level = current_level + 1
+    for x in range(len(triangle[current_level])):
+        a = triangle[lower_level][x]
+        b = triangle[lower_level][x+1]
+        if a >= b:
+            triangle[current_level][x]+=a
+        else:
+            triangle[current_level][x]+=b
+    #go up a level
+    current_level -= 1
+
+print(triangle[0])
